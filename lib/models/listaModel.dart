d356fe349afb8bf6dee944b424408209ddb5743d
Alain <alain.andres@ext.aqualogybs.net>
2020-11-20 00:21:00 +0100

    Mostrando listas desde bd con fecha parseada

--- lib/models/listaModel.dart
+++ lib/models/listaModel.dart
@@ -1,16 +1,22 @@
 class ListaModel {
   final int id;
   final String name;
+  final int createdAt;
+  final int updatedAt;
 
   ListaModel({
     this.id,
     this.name,
+    this.createdAt,
+    this.updatedAt,
   });
 
   factory ListaModel.fromJson(Map<String, dynamic> json) {
     return ListaModel(
       id: json['id'],
-      name: json['name'],
+      name: json['name'] as String,
+      createdAt: json['created_at'],
+      updatedAt: json['updated_at'],
     );
   }
 
