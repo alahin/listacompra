479a450851d215455a7da007e085338b33b6dadf
Alain <alain.andres@ext.aqualogybs.net>
2020-11-22 20:11:01 +0100

    Function delete Lista

--- lib/service/listaService.dart
+++ lib/service/listaService.dart
@@ -4,13 +4,10 @@ import 'package:http/http.dart' as http;
 import 'package:listacompra/constants/constants.dart' as Constants;
 
 class ListaService {
-  Future<String> add(ListaModel listaModel) async {
-    final response = await http.post(
-      Constants.URI_LISTAS,
-      body: listaModel.toJsonAdd(),
-    );
+  Future<String> add(ListaModel model) async {
+    final response =
+        await http.post(Constants.URI_LISTAS, body: model.toJsonAdd());
     if (response.statusCode == 200) {
-      print(response.body);
       return response.body;
     } else {
       return "Error";
@@ -23,6 +20,15 @@ class ListaService {
       return listFromJson(response.body);
     } else {
       return List<ListaModel>();
+    }
+  }
+
+  Future<String> deleteLista(int id) async {
+    final response = await http.delete(Constants.URI_LISTAS + '/$id');
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return "Error";
     }
   }
 
--- lib/ui/homePage.dart
+++ lib/ui/homePage.dart
@@ -3,8 +3,8 @@ import 'package:listacompra/models/listaModel.dart';
 import 'package:listacompra/service/listaService.dart';
 import 'package:listacompra/ui/addListPage.dart';
 import 'package:intl/intl.dart';
-
 import 'detailsListPage.dart';
+import 'package:toast/toast.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -23,6 +23,12 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  deleteLista(int id) async {
+    await ListaService().deleteLista(id);
+    Toast.show("Eliminado", context, gravity: Toast.CENTER, duration: 2);
+    getAllListas();
+  }
+
   @override
   void initState() {
     super.initState();
@@ -32,30 +38,30 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Lista de compra"),
-          actions: <Widget>[
-            IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => AddListPage(),
-                  ),
-                ).then((value) => getAllListas());
-              },
-              icon: Icon(Icons.add),
-            ),
-          ],
-        ),
-        body: loading
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : ListView.builder(
-                itemCount: listasList.length,
-                itemBuilder: (context, index) {
-                  ListaModel lista = listasList[index];
+      appBar: AppBar(
+        title: Text("Lista de compra"),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddListPage(),
+                ),
+              ).then((value) => getAllListas());
+            },
+            icon: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: loading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: listasList.length,
+              itemBuilder: (context, index) {
+                /*ListaModel lista = listasList[index];
                   return ListTile(
                     title: Text(lista.name),
                     subtitle: Text(df.format(
@@ -69,8 +75,69 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ).then((value) => getAllListas());
                     },
-                  );
-                },
-              ));
+                  );*/
+                return new InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailsListPage(),
+                      ),
+                    ).then((value) => getAllListas());
+                  },
+                  child: Container(
+                    height: 50.0,
+                    decoration: BoxDecoration(),
+                    child: new Column(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.only(
+                            left: 15.0,
+                            right: 15.0,
+                          ),
+                          child: new Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              new Container(
+                                child: Text(
+                                  listasList[index].name,
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(fontSize: 18.0),
+                                  maxLines: 1,
+                                ),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(10.0),
+                                    topRight: Radius.circular(10.0),
+                                  ),
+                                ),
+                              ),
+                              new IconButton(
+                                  splashColor: Colors.red,
+                                  icon: Icon(Icons.delete),
+                                  onPressed: () {
+                                    deleteLista(listasList[index].id);
+                                  }),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(
+                            left: 15.0,
+                            right: 15.0,
+                            top: 0.0,
+                          ),
+                          child: Container(
+                            color: Colors.black,
+                            height: 0.2,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+    );
   }
 }
